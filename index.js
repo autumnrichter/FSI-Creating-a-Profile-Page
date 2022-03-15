403858b87eafdbf2350eb1fda05a1639c629be49
Autumn Richter <autumn.richter@gmail.com>
2022-03-14 20:31:30 -0400

    Started setting dogDetails

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 let content = document.querySelector('.js-generated.content')
+//H1
+let mainHeader = document.createElement('h1')
+mainHeader.setAttribute('class', 'dog-name')
+mainHeader.textContent = 'Rizzo'
+content.append(mainHeader)
+
+//Make a dogContent div 
+let dogContentDiv = document.createElement('div')
+dogContentDiv.setAttribute('class', 'dog-content')
+content.append(dogContentDiv)
+
+//append an image
+let dogContentImage = document.createElement('img')
+dogContentImage.setAttribute('class', 'dog-image')
+dogContentImage.setAttribute('src', './assets/rizzo.jpg')
+dogContentDiv.append(dogContentImage)
+
+//Make a dogDetails div
+let dogDetailsDiv = document.createElement('div')
+dogDetailsDiv.setAttribute('class', 'dog-details')
