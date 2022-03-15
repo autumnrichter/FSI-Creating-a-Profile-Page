7953feefd77048cb3166e4de4082d7909ecbb210
Autumn Richter <autumn.richter@gmail.com>
2022-03-14 23:36:34 -0400

    added ul and li items. Completed activity.

--- index.js
+++ index.js
@@ -16,7 +16,41 @@ dogContentImage.setAttribute('class', 'dog-image')
 dogContentImage.setAttribute('src', './assets/rizzo.jpg')
 dogContentDiv.append(dogContentImage)
 
-//Make a dogDetails div
+//Make a dogDetails div under dogContentDiv
 let dogDetailsDiv = document.createElement('div')
 dogDetailsDiv.setAttribute('class', 'dog-details')
-dogDetailsHeader = document.createElement('h3')
+dogContentDiv.append(dogDetailsDiv)
+
+//Create H3 under dogDetailsDiv
+let dogDetailsHeader = document.createElement('h3')
+dogDetailsHeader.textContent = 'Description:'
+dogDetailsDiv.append(dogDetailsHeader)
+
+//Create P under dogDetailsDiv
+let paragraph = document.createElement('p')
+paragraph.textContent = 'This gentile dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.'
+dogDetailsDiv.append(paragraph)
+
+//Create H3 under dogDetailsDiv
+let feedingTimesHeader = document.createElement('h3')
+feedingTimesHeader.textContent = 'Feeding Times:'
+dogDetailsDiv.append(feedingTimesHeader)
+
+//Create UL under dogDetailsDiv
+let ul = document.createElement('ul')
+dogDetailsDiv.append(ul)
+
+//Create first LI in UL
+let li1 = document.createElement('li')
+li1.textContent = '9:00 am'
+ul.append(li1)
+
+//Create second LI in UL
+let li2 = document.createElement('li')
+li2.textContent = '12:00 pm'
+ul.append(li2)
+
+//Create third LI in UL
+let li3 = document.createElement('li')
+li3.textContent = '5:00 pm'
+ul.append(li3)
